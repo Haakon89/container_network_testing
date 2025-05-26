58166dec731e137ebdd40ca0e6121732d2633c22
Håkon Sørensen <haakonsorensen89@gmail.com>
2025-05-26 11:55:23 +0200

    Added a java project for network simulation

---
+++ network_sim/src/main/java/network/simulation/test/App.java
@@ -0,0 +1,13 @@
+package network.simulation.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
